ed2677b2f2810c50151032a866fff3d24c80a217
Duncan Schmets <2404512schmets@zuyd.nl>
2025-05-23 11:49:01 +0200

    Laatste effort om bestelapplicatie werkend te krijgen

---
+++ KE03_INTDEV_SE_1_Base/Pages/Products.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KE03_INTDEV_SE_1_Base.Pages
+{
+    public class ProductsModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IProductRepository _productRepository;
+
+        public IList<Product> Products { get; set; }
+
+        public ProductsModel(ILogger<IndexModel> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            this._productRepository = productRepository;
+            Products = new List<Product>();
+        }
+
+        public void OnGet()
+        {
+            Products = _productRepository.GetAllProducts().ToList();
+            _logger.LogInformation($"getting all {Products.Count} products");
+        }
+    }
+}
