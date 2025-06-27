d7b277aa7a189265bbf9f84d07f284bb4be6da79
Duncan Schmets <2404512schmets@zuyd.nl>
2025-06-27 11:57:58 +0200

    ProductDetails pagina layout

--- KE03_INTDEV_SE_1_Base/Pages/ProductDetails.cshtml.cs
+++ KE03_INTDEV_SE_1_Base/Pages/ProductDetails.cshtml.cs
@@ -1,10 +1,14 @@
+using DataAccessLayer;
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
-using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace KE03_INTDEV_SE_1_Base.Pages
+namespace KE03_INTDEV_SE_1_Base.Pages.Shared
 {
     public class ProductDetailsModel : PageModel
     {
@@ -15,14 +19,12 @@ namespace KE03_INTDEV_SE_1_Base.Pages
             _context = context;
         }
 
-        public Product? Product { get; set; }
+        public Product Product { get; set; }
+        public List<Product> RelevantProducts { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            Product = await _context.Products
-                .Include(p => p.Orders)
-                .Include(p => p.Parts)
-                .FirstOrDefaultAsync(p => p.Id == id);
+            Product = await _context.Products.FindAsync(id);
 
             if (Product == null)
             {
@@ -30,6 +32,45 @@ namespace KE03_INTDEV_SE_1_Base.Pages
             }
 
             return Page();
+        }
+
+        public IActionResult OnPostAddToCart(int productId, int quantity)
+        {
+            var product = _context.Products.Find(productId);
+            if (product == null || quantity <= 0)
+            {
+                return BadRequest("Invalid product or quantity.");
+            }
+
+            var cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            var cartItem = cart.FirstOrDefault(ci => ci.Product.Id == productId);
+            if (cartItem != null)
+            {
+                cartItem.Quantity += quantity;
+            }
+            else
+            {
+                cart.Add(new CartItem { Product = product, Quantity = quantity });
+            }
+
+            HttpContext.Session.SetObjectAsJson("Cart", cart);
+
+            return RedirectToPage("Cart");
+        }
+    }
+
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
         }
     }
 }
